2bd9ba820e247822b152c068572a2e8fac8f63e4
consoleLogIt <kishansingh4do@gmail.com>
2020-10-15 15:05:28 +0530

    user login - I

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,28 @@ import React, { Component } from 'react';
 class Login extends Component {
     constructor(props){
         super(props);
-        this.emailInputRef = React.createRef();
-        this.passwordInputRef = React.createRef();
+        // this.emailInputRef = React.createRef();
+        // this.passwordInputRef = React.createRef();
+        this.state = {
+            email:'',
+            password:''
+        }
     }
     handleFormSubmit = () => {
-        console.log("this.emailRef",this.emailInputRef);
-        console.log("this.passRef",this.passwordInputRef);
+        // console.log("this.emailRef",this.emailInputRef);
+        // console.log("this.passRef",this.passwordInputRef);
+        console.log(this.state);
 
+    }
+    handleEmailChange = (e) => {
+        this.setState({
+            email:e.target.value
+        })
+    }
+    handlePasswordChange = (e) => {
+        this.setState({
+            password:e.target.value
+        })
     }
     render() {
         return (
@@ -17,10 +32,10 @@ class Login extends Component {
                     <form className="login-form">
                <span className = "login-signup-header">Log In</span>
                <div className= "field">
-                   <input type = "email" placeholder = "Email" required  ref = {this.emailInputRef}/>
+                   <input type = "email" placeholder = "Email" required onChange = {this.handleEmailChange}/>
                    </div>
                    <div className="field">
-                   <input type = "password" placeholder = "Password" required ref = {this.passwordInputRef} />
+                   <input type = "password" placeholder = "Password" required onChange = {this.handlePasswordChange} />
                    </div>
                    <div className="field">
                        <button onClick = {this.handleFormSubmit}>Log In</button>
